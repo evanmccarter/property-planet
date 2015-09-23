8ad7a24bdc57a7685519428d8646cd4648b43ca2
Evan McCarter <evan.mccarter@gmail.com>
2015-09-23 01:15:32 -0400

    changed indentation to tabs, changed name of "value" variable, added 3 extra variables to js, html, and sol

--- app/js/index.js
+++ app/js/index.js
@@ -1,19 +1,25 @@
 $(document).ready(function() {
 
-  $("button.set").click(function() {
-    var value = parseInt($("input.text").val(), 10);
-    SimpleStorage.set(value);
-    addToLog("SimpleStorage.set("+value+")");
-  });
+	$("button.set").click(function() {
+		var lat1 = parseInt($("input.lat1").val(), 10);
+		var lat2 = parseInt($("input.lat2").val(), 10);
+		var long1 = parseInt($("input.long1").val(), 10);
+		var long2 = parseInt($("input.long2").val(), 10);
+		SimpleStorage.set(lat1, lat2, long1, long2);
+		addToLog("SimpleStorage.set("+lat1+", "+lat2+", "+long1+", "+long2+")");
+	});
 
-  document.getElementsByClassName("get")[0].addEventListener('click', function() {
-    var value = SimpleStorage.get().toNumber();
-    $(".value").html(value);
-    addToLog("SimpleStorage.get()");
-  });
+	document.getElementsByClassName("get")[0].addEventListener('click', function() {
+		var glat1 = SimpleStorage.getla1().toNumber();
+		var glat2 = SimpleStorage.getla2().toNumber();
+		var glon1 = SimpleStorage.getlo1().toNumber();
+		var glon2 = SimpleStorage.getlo2().toNumber();
+		$(".value").html(glat1+", "+glat2+", "+glon1+", "+glon2);
+		addToLog("SimpleStorage.get()");
+	});
 
-  var addToLog = function(txt) {
-    $(".logs").append("<br>" + txt);
-  }
+	var addToLog = function(txt) {
+		$(".logs").append("<br>" + txt);
+	}
 
 });
--- dist/contracts/app/contracts/simple_storage.sol
+++ dist/contracts/app/contracts/simple_storage.sol
@@ -1,14 +1,45 @@
 contract SimpleStorage {
-  uint public storedData;
+	uint public lat1;
+	uint public lat2;
+	uint public long1;
+	uint public long2;
 
-  function SimpleStorage(uint initialValue) {
-    storedData = initialValue;
-  }
+	function SimpleStorage(uint initialValue) {
+		lat1 = initialValue;
+		lat2 = initialValue;
+		long1 = initialValue;
+		long2 = initialValue;
+	}
 
-  function set(uint x) {
-    storedData = x;
-  }
-  function get() constant returns (uint retVal) {
-    return storedData;
-  }
+	function set(uint x) {
+		lat1 = x;
+	}
+	
+	function set(uint nlat1, uint nlat2, uint nlong1, uint nlong2) {
+		lat1 = nlat1;
+		lat2 = nlat2;
+		long1 = nlong1;
+		long2 = nlong2;
+	}
+	
+	function get() constant returns (uint retVal) {
+		return lat1;
+	}
+	
+	
+	function getla1() constant returns (uint retVal) {
+		return lat1;
+	}
+	
+	function getla2() constant returns (uint retVal) {
+		return lat2;
+	}
+	
+	function getlo1() constant returns (uint retVal) {
+		return long1;
+	}
+	
+	function getlo2() constant returns (uint retVal) {
+		return long2;
+	}
 }
